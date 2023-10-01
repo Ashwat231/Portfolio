417ff33cc2e427ff0ecceda157fb16ddb4fe9603
Ashwat231 <ashwatrajbhandari5@gmail.com>
2023-10-01 16:10:13 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,195 @@
-import Image from 'next/image'
+"use client";
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+import { AiFillInstagram, AiFillLinkedin,AiOutlineMobile,AiFillGithub,AiOutlineArrowUp} from 'react-icons/ai';
+import {TbBrandNextjs} from 'react-icons/tb';
+import ReactCardFlip from 'react-card-flip';
+import {BiLogoReact,BiLogoAngular,BiLogoTailwindCss,BiLogoWordpress,BiLogoPython} from 'react-icons/bi';
+import React, { useState,useRef } from 'react';
+import emailjs, { send } from '@emailjs/browser';
+
 
 export default function Home() {
+
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState('');
+  const [text3, setText3] = useState('');  
+  const [isVisible, setIsVisible] = useState(false);
+  const [color,setColor] = useState(true);
+  const [isFlipped,setIsFlipped] = useState(false);
+  const form = useRef();
+
+  function handleClear1() {
+    setText1('');
+  }
+  function handleClear2() {
+    setText2('');
+  }
+  function handleClear3() {
+    setText3('');
+  }
+  
+
+  function sendEmail(e){
+    alert('Message Successfully Sent');
+    e.preventDefault(); 
+    emailjs.sendForm('service_zzczf1f', 'template_ff1oyb9', form.current, 'bjfPNuA27thibvI6x')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+  
+  function flipCard(){
+    setIsFlipped(!isFlipped);
+  }
+
+  const toggleNavbar = () => {
+    setIsVisible(!isVisible);
+  };
+
+  const toggleColor = () =>{
+    setColor(!color)
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <html className=' scroll-smooth'>
+    <body>
+<section className="min-h-screen bg-gradient-to-b from-gray-900 to-gray-700 flex flex-col justify-between px-10 max-sm:px-0" id='about-me'>
+
+<nav className='py-10 mb-12 flex justify-between max-sm:pb-0 max-sm:px-5' id='about-me'>
+  <h1 className={`text-2xl ${color ? 'text-teal-600' : 'text-yellow-600'} font-bold max-sm:text-lg`}><a href='#about-me'>PORTFOLIO</a></h1>
+  <ul className='flex gap-20 text-sm tracking-widest max-md:hidden'>
+    <li> <a href="#about-me" className=' font-bold text-white hover:text-xl duration-500'>ABOUT ME</a></li>
+    <li> <a href="#skills" className=' font-bold text-white hover:text-xl duration-500'>SKILLS</a></li>
+    <li> <a href="#projects" className=' font-bold text-white hover:text-xl duration-500'>PROJECTS</a></li>
+  </ul>
+  <ul className='flex items-center'>
+    <li>  <BsFillMoonStarsFill className='cursor-pointer text-2xl text-yellow-400 max-md:hidden' onClick={toggleColor}/></li>
+    <li>  <a href="/Ashwat_Rajbhandari_Resume.pdf" download='Ashwat_Rajbhandari_Resume' className='max-sm:text-xs bg-gradient-to-l from-cyan-500 to-teal-500 py-2 px-4 max-sm:px-2  rounded-lg text-white ml-8'>
+      Resume
+    </a>  </li>
+    <li className='hidden cursor-pointer py-2 mx-4 rounded-lg text-white  rotate-90 max-md:block max-sm:text-xs' id='hamburger'><button onClick={toggleNavbar}>|||</button></li>
+  </ul>
+</nav>
+
+<nav className={`max-sm:-mt-8 max-sm:mb-5 max-sm:mx-10 md:hidden gap-5 -mt-16 mb-10 bg-gradient-to-b from-gray-900 to-gray-700 overflow-hidden transition-all duration-1000 ${isVisible ? 'h-64 opacity-100' : 'h-0 opacity-0'} rounded-full`}>
+  <ul>
+    <li className=' hover:text-xl hover:bg-gradient-to-r from-cyan-500 to-teal-500  p-5 text-white text-center'><a href='#about-me' onClick={ ()=>{ setIsVisible(false)}}>ABOUT ME</a></li>
+    <li className=' hover:text-xl hover:bg-gradient-to-r from-cyan-500 to-teal-500  p-5 text-white text-center'><a href='#skills' onClick={ ()=>{ setIsVisible(false)}}>SKILLS</a></li>
+    <li className=' hover:text-xl hover:bg-gradient-to-r from-cyan-500 to-teal-500  p-5 text-white text-center'><a href='#projects' onClick={( )=>{ setIsVisible(false)}}>PROJECTS</a></li>
+    <li className=' hover:text-xl p-5 flex justify-center'><BsFillMoonStarsFill className=' cursor-pointer text-3xl text-yellow-400' onClick={toggleColor}/></li>
+  </ul>
+</nav>
+
+<div className='flex justify-around max-md:flex-col max-sm:px-10'>
+
+  <div>
+    <img src='Me.jpg' className='rounded-full max-md:mb-10  max-md:mx-auto'></img>
+  </div>
+
+<div className='w-1/2 max-md:w-auto'>
+  <ReactCardFlip flipDirection='horizontal' isFlipped={isFlipped} flipSpeedBackToFront={1} flipSpeedFrontToBack={1}>
+  <div className= {`border-x-2 border-b-2 ${color? 'border-teal-600 shadow-lg shadow-teal-600' : 'border-yellow-600 shadow-lg shadow-yellow-600'}  rounded-2xl p-8 flex flex-col justify-around max-md:w-full`}>
+    <p className='text-white max-md:p-2 pb-5'>Hi, my name is</p>
+    <h1 className={`text-5xl pb-5 max-md:p-2 ${ color ? 'text-teal-500': 'text-yellow-600'} font-bold max-sm:text-3xl`}>ASHWAT RAJBHANDARI</h1>
+    <h3 className=' text-2xl pb-5 max-md:p-2 text-white max-sm:text-lg'>BTech Computer Science & Engineering | Specialization In IOT | Software Engineer </h3>
+    <h3 className='text-white pb-5 max-md:p-2 max-sm:text-xs'>I am a graduate in Computer Science Engineering with specialization in Internet of things. Interested in AI and web development, looking to gain more experience and knowledge from working and learning.</h3>
+    <a href = '#' className='text-yellow-400 font-bold text-right max-md:py-2 max-md:text-sm' onClick={(e)=>{
+      flipCard();
+      e.preventDefault();
+      }}> Contact Me -) </a>
+  </div>
+
+  <div className= {`border-x-2 border-b-2 ${color? 'border-teal-600 shadow-lg shadow-teal-600' : 'border-yellow-600 shadow-lg shadow-yellow-600'}  rounded-2xl p-8 flex flex-col justify-around max-md:w-full w-4/5 max-sm:p-5`}>
+    <h1 className={`font-bold text-teal-500 text-3xl mb-5 max-sm:text-xl ${ color ? 'text-teal-500': 'text-yellow-600'}`}>Write Me A Message</h1>
+    <form className='text-white max-sm:text-sm' ref={form} onSubmit={sendEmail}>
+
+      <label className='flex-col flex font-bold'>
+        Your Name
+      <input type='text' value={text1} onChange={(e)=>{setText1(e.target.value)}} name='from_name' className='border-2 border-teal-600 my-3 text-black rounded-full h-10 p-5'/>
+      </label>
+
+      <label className='flex-col flex font-bold'>
+        Your Email Address
+      <input type='text' value={text2} onChange={(e)=>{setText2(e.target.value)}} name='from-email' className='border-2 border-teal-600 my-3 text-black rounded-full h-10 p-5'/>
+      </label>
+
+      <label className='flex-col flex font-bold'>
+        Your Message
+        <textarea rows="4" cols="50" value={text3} onChange={(e)=>{setText3(e.target.value)}} name='message' className='border-2 border-teal-600 my-3 text-black p-2 rounded-lg'/>
+      </label>
+
+      <button className=' mx-2 bg-gradient-to-l from-cyan-500 to-teal-500 px-5 py-2 border-2 border-black mt-5 rounded-full' type='submit'>SEND</button>
+      <button className=' mx-2 bg-gradient-to-l from-yellow-700 to-yellow-500 px-5 py-2 border-2 border-black mt-5 rounded-full' type='button' onClick={()=>{handleClear1();handleClear2();handleClear3();}}>CLEAR</button>
+
+
+    </form>
+    <a href = '#' className='text-yellow-400 font-bold text-right max-md:py-2 max-md:text-sm mt-5' onClick={(e)=>{
+      e.preventDefault();
+      flipCard();
+    }}> About Me -) </a>
+  </div>
+  </ReactCardFlip>
+</div>
+
+
+</div>
+
+<div className='py-10 text-4xl gap-12 flex justify-center'>
+<a href='https://github.com/Ashwat231'><AiFillGithub className='text-white cursor-pointer hover:text-black duration-500'/></a>
+      <a href='https://www.linkedin.com/in/ashwat-rajbhandari-b9b809194/'><AiFillLinkedin className='text-white cursor-pointer hover:text-blue-600 duration-500'/></a>
+      <a href='https://www.instagram.com/_ashwat_/'><AiFillInstagram className='text-white cursor-pointer hover:text-orange-600 duration-500'/></a> 
+</div>
+
+</section>
+
+
+      <section className='px-10 my-16' id='skills'>
+          <h3 className={`text-3xl py-1 w-1 text-teal-600 font-bold ${color ? 'text-teal-500': 'text-yellow-600'}`}>Skills</h3>
+          <div class="border-b-2 border-gray-400 my-4"></div>
+          <p className='font-bold'>Some of the things I have worked with:</p>
+          <div className='p-10 m-5 h-auto shadow-2xl border-black rounded-3xl bg-gradient-to-t from-gray-900 to-gray-700 flex flex-col w-auto'>
+            <ul className='flex justify-between items-center mb-10 max-md:overflow-hidden flex-wrap max-as:justify-center'>
+              <li className=' text-center text-white mx-5 my-5'><BiLogoReact className='text-8xl text-blue-500 max-md:text-7xl' /> React JS</li>
+              <li className=' text-center text-white mx-5 my-5'><BiLogoPython className='text-8xl text-yellow-500 max-md:text-7xl'/> Python</li>
+              <li className=' text-center text-white mx-5 my-5'><AiFillGithub className='text-8xl text-white max-md:text-7xl'/> Github</li>
+              <li className=' text-center text-white mx-5 my-5'><BiLogoTailwindCss className='text-8xl text-blue-500 max-md:text-7xl'/> TAILWIND</li>
+
+            </ul>
+            <ul className='flex justify-between items-center max-md:overflow-hidden flex-wrap max-as:justify-center'>
+              <li className=' text-center text-white mx-5 my-5'><BiLogoAngular className='text-8xl text-red-900 max-md:text-7xl'/> Angular JS</li>
+              <li className=' text-center text-white mx-5 my-5'><TbBrandNextjs className='text-8xl text-white max-md:text-7xl'/> Next JS</li>
+              <li className=' text-center text-white mx-5 my-5'><AiOutlineMobile className='text-8xl text-orange-500 max-md:text-7xl'/> React <br/>Native</li>
+              <li className=' text-center text-white mx-5 my-5'><BiLogoWordpress className='text-8xl text-gray-600 max-md:text-7xl'/> Wordpress</li>
+
+            </ul>
+          </div>
+      </section>
+
+      <section className='px-10 my-16' id='projects'>
+          <h3 className={`text-3xl py-1 text-teal-600 font-bold ${color ? 'text-teal-500': 'text-yellow-600'}`}>Projects</h3>
+          <div class="border-b-2 border-gray-400 my-4"></div>
+
+          <div className='p-10 m-5 h-auto shadow-2xl bg-gradient-to-t from-gray-900 to-gray-700 rounded-3xl flex justify-center items-center'>
+            <p className=' text-3xl text-center text-white'>COMING SOON</p>
+          </div>
+          <a href='#' className='absolute right-1 mb-36'><AiOutlineArrowUp className=' text-4xl text-yellow-500 p-2 bg-black rounded-full max-as:text-4xl'/></a>
+
+      </section>
+
+
+    <footer className='p-5 bg-gradient-to-b from-gray-900 to-gray-700 text-white text-center flex flex-col gap-5'>
+
+      <p>Created With NextJS & TailwindCSS</p>
+      <div className='flex justify-center gap-10'>
+      <a href='https://github.com/Ashwat231'><AiFillGithub className='text-white cursor-pointer hover:text-black text-5xl duration-500'/></a>
+      <a href='https://www.linkedin.com/in/ashwat-rajbhandari-b9b809194/'><AiFillLinkedin className='text-white cursor-pointer hover:text-blue-600 text-5xl duration-500'/></a>
+      <a href='https://www.instagram.com/_ashwat_/'><AiFillInstagram className='text-white cursor-pointer hover:text-orange-600 text-5xl duration-500'/></a> 
       </div>
+    </footer>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </body>
+    </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens:{
+      'md':'930px',
+      'sm':'450px',
+      'as':'785px',
     },
   },
   plugins: [],
